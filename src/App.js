0022c42256749d00c89ff758724bd233ddf7989e
Ahmed Ali <eng.ahmed.ali.1998@gmail.com>
2022-12-28 10:54:01 -0800

    change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     
      <>
-     <h1 className="title"  >Ahmed Ali</h1>
+     <h1 className="title"  >Ahmed Ali 1998</h1>
      <h2 className="title">REACT </h2>
      </>
   
